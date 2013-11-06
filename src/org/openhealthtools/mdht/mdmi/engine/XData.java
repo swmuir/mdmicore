766d0cec3f9c0feba4e69fcfe9e4255a83962271
seanmuir <seanmuir@b3b0ff50-1bf6-4f1c-ad2b-09b492ebfb26>
2013-11-06 14:26:51 +0000

    Added additional information for when datatype is null in ctor

--- src/org/openhealthtools/mdht/mdmi/engine/XData.java
+++ src/org/openhealthtools/mdht/mdmi/engine/XData.java
@@ -7,6 +7,7 @@
 *
 * Contributors:
 *     Firestar Software, Inc. - initial API and implementation
+* 
 *
 * Author:
 *     Gabriel Oancea
@@ -14,7 +15,7 @@
 *******************************************************************************/
 package org.openhealthtools.mdht.mdmi.engine;
 
-import org.openhealthtools.mdht.mdmi.model.*;
+import org.openhealthtools.mdht.mdmi.model.DTComplex;
 
 /** 
  * Base class for the structure and choice data types.
@@ -23,50 +24,54 @@ import org.openhealthtools.mdht.mdmi.model.*;
  * @author goancea
  */
 abstract class XData {
-   protected XValue    m_owner   ;
-   protected DTComplex m_datatype;
+	protected XValue m_owner;
 
-   /**
-    * The owner may be null in some circumstances, the data type may not.
-    * 
-    * @param owner The value owner.
-    * @param datatype The data type (can be DTCStructured or DTCChoice).
-    */
-   public XData( XValue owner, DTComplex datatype ) {
-      if( datatype == null )
-         throw new IllegalArgumentException( "Null datatype!" );
-      m_datatype = datatype;
-      m_owner    = owner   ;
-   }
+	protected DTComplex m_datatype;
 
-   /**
-    * Get the owner value.
-    * 
-    * @return The owner value.
-    */
-   public XValue getOwner() {
-      return m_owner;
-   }
-   
-   /**
-    * Get the data type (can be DTCStructured or DTCChoice).
-    * 
-    * @return The data type (can be DTCStructured or DTCChoice).
-    */
-   public DTComplex getDatatype() {
-      return m_datatype;
-   }
-   
-   @Override
-   public String toString() {
-      return toString( "" );
-   }
-   
-   /**
-    * Build the string representation of this value.
-    * 
-    * @param indent The indentation to use.
-    * @return The string representation of this value.
-    */
-   protected abstract String toString( String indent );
+	/**
+	 * The owner may be null in some circumstances, the data type may not.
+	 * 
+	 * @param owner The value owner.
+	 * @param datatype The data type (can be DTCStructured or DTCChoice).
+	 */
+	public XData(XValue owner, DTComplex datatype) {
+		if (datatype == null) {
+			throw new IllegalArgumentException((owner != null
+					? owner.getName()
+					: "NULL OWNER") + " has null datatype!");
+		}
+		m_datatype = datatype;
+		m_owner = owner;
+	}
+
+	/**
+	 * Get the owner value.
+	 * 
+	 * @return The owner value.
+	 */
+	public XValue getOwner() {
+		return m_owner;
+	}
+
+	/**
+	 * Get the data type (can be DTCStructured or DTCChoice).
+	 * 
+	 * @return The data type (can be DTCStructured or DTCChoice).
+	 */
+	public DTComplex getDatatype() {
+		return m_datatype;
+	}
+
+	@Override
+	public String toString() {
+		return toString("");
+	}
+
+	/**
+	 * Build the string representation of this value.
+	 * 
+	 * @param indent The indentation to use.
+	 * @return The string representation of this value.
+	 */
+	protected abstract String toString(String indent);
 } // XData
