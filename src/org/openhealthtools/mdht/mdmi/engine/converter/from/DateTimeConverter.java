2a28087f6e021278083577593647837e6b500ede
yaroslavTIr <yaroslavTIr@b3b0ff50-1bf6-4f1c-ad2b-09b492ebfb26>
2013-12-20 14:08:42 +0000

    Artifact artf3676 : Error on Date Format (https://www.projects.openhealthtools.org/sf/go/artf3676?nav=1&_pagenum=1&returnUrlKey=1386258307703)

--- src/org/openhealthtools/mdht/mdmi/engine/converter/from/DateTimeConverter.java
+++ src/org/openhealthtools/mdht/mdmi/engine/converter/from/DateTimeConverter.java
@@ -18,54 +18,50 @@ import java.text.*;
 import java.util.Date;
 
 import org.openhealthtools.mdht.mdmi.*;
+import org.openhealthtools.mdht.mdmi.engine.converter.DateWrapper;
 import org.openhealthtools.mdht.mdmi.util.*;
 
 public class DateTimeConverter implements IConvertFromString {
 
-
-    private Object parseDateYMD(String value) throws ParseException {
-        String formatString = "yyyyMMdd";
-        SimpleDateFormat sdf = new SimpleDateFormat(formatString);
-        return sdf.parse(value);
-    }
-
     @Override
     public Object convertFromString(String value, String format) {
-        if( format != null && 0 < format.length() ) {
+        if (format != null && 0 < format.length()) {
             String[] formatStrings = format.split(";");
             for (String formatString : formatStrings) {
                 try {
                     SimpleDateFormat sdf = new SimpleDateFormat(formatString);
-                    return sdf.parse(value);
-                }
-                catch (ParseException ignored) {}
-                catch( Exception ex ) {
+                    return new DateWrapper(sdf.parse(value), formatString, value);
+                } catch (ParseException ignored) {
+                } catch (Exception ex) {
                     throw new MdmiException(ex, "DateTimeConverter.convertFromString({0}, {1}) failed.", value, formatString);
                 }
             }
         }
 
         try {
-            return XmlUtil.parseDateYMDHMSMZ(value);
+            return new DateWrapper(XmlUtil.parseDateYMDHMSMZ(value), "YMDHMSMZ", value);
         } catch (Exception ignored) {
         }
         try {
-            return XmlUtil.parseDateYMDHMSZ(value);
+            return new DateWrapper(XmlUtil.parseDateYMDHMSZ(value), "YMDHMSZ", value);
         } catch (Exception ignored) {
         }
         try {
-            return XmlUtil.parseDateYMDZ(value);
+            return new DateWrapper(XmlUtil.parseDateYMDZ(value), "YMDZ", value);
         } catch (Exception ignored) {
         }
-        try{
-            return parseDateYMD(value);
+        try {
+            return new DateWrapper(parseDateYMD(value), "YMD", value);
         } catch (Exception ignored) {
         }
         throw new MdmiException("DateTimeConverter.convertFromString({0}, {1}) failed.", value, format);
     }
 
 
-    public static void main(String[] args) throws ParseException {
-        System.out.println(XmlUtil.parseDateYMDHMSMZ("20000407130000+0500"));
+    private Date parseDateYMD(String value) throws ParseException {
+        String formatString = "yyyyMMdd";
+        SimpleDateFormat sdf = new SimpleDateFormat(formatString);
+        return sdf.parse(value);
     }
+
 }
