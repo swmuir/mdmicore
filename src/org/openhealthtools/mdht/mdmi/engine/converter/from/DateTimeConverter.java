6756e06daac9e6e0dfc45db953faaf378177f279
yaroslavTIr <yaroslavTIr@b3b0ff50-1bf6-4f1c-ad2b-09b492ebfb26>
2013-12-21 12:26:19 +0000

    Revert: Artifact artf3676 : Error on Date Format (https://www.projects.openhealthtools.org/sf/go/artf3676?nav=1&_pagenum=1&returnUrlKey=1386258307703)

--- src/org/openhealthtools/mdht/mdmi/engine/converter/from/DateTimeConverter.java
+++ src/org/openhealthtools/mdht/mdmi/engine/converter/from/DateTimeConverter.java
@@ -18,50 +18,54 @@ import java.text.*;
 import java.util.Date;
 
 import org.openhealthtools.mdht.mdmi.*;
-import org.openhealthtools.mdht.mdmi.engine.converter.DateWrapper;
 import org.openhealthtools.mdht.mdmi.util.*;
 
 public class DateTimeConverter implements IConvertFromString {
 
+
+    private Object parseDateYMD(String value) throws ParseException {
+        String formatString = "yyyyMMdd";
+        SimpleDateFormat sdf = new SimpleDateFormat(formatString);
+        return sdf.parse(value);
+    }
+
     @Override
     public Object convertFromString(String value, String format) {
-        if (format != null && 0 < format.length()) {
+        if( format != null && 0 < format.length() ) {
             String[] formatStrings = format.split(";");
             for (String formatString : formatStrings) {
                 try {
                     SimpleDateFormat sdf = new SimpleDateFormat(formatString);
-                    return new DateWrapper(sdf.parse(value), formatString, value);
-                } catch (ParseException ignored) {
-                } catch (Exception ex) {
+                    return sdf.parse(value);
+                }
+                catch (ParseException ignored) {}
+                catch( Exception ex ) {
                     throw new MdmiException(ex, "DateTimeConverter.convertFromString({0}, {1}) failed.", value, formatString);
                 }
             }
         }
 
         try {
-            return new DateWrapper(XmlUtil.parseDateYMDHMSMZ(value), "YMDHMSMZ", value);
+            return XmlUtil.parseDateYMDHMSMZ(value);
         } catch (Exception ignored) {
         }
         try {
-            return new DateWrapper(XmlUtil.parseDateYMDHMSZ(value), "YMDHMSZ", value);
+            return XmlUtil.parseDateYMDHMSZ(value);
         } catch (Exception ignored) {
         }
         try {
-            return new DateWrapper(XmlUtil.parseDateYMDZ(value), "YMDZ", value);
+            return XmlUtil.parseDateYMDZ(value);
         } catch (Exception ignored) {
         }
-        try {
-            return new DateWrapper(parseDateYMD(value), "YMD", value);
+        try{
+            return parseDateYMD(value);
         } catch (Exception ignored) {
         }
         throw new MdmiException("DateTimeConverter.convertFromString({0}, {1}) failed.", value, format);
     }
 
 
-    private Date parseDateYMD(String value) throws ParseException {
-        String formatString = "yyyyMMdd";
-        SimpleDateFormat sdf = new SimpleDateFormat(formatString);
-        return sdf.parse(value);
+    public static void main(String[] args) throws ParseException {
+        System.out.println(XmlUtil.parseDateYMDHMSMZ("20000407130000+0500"));
     }
-
 }
--- src/org/openhealthtools/mdht/mdmi/engine/converter/to/ToDateTimeConverter.java
+++ src/org/openhealthtools/mdht/mdmi/engine/converter/to/ToDateTimeConverter.java
@@ -1,60 +1,48 @@
 /*******************************************************************************
- * Copyright (c) 2012 Firestar Software, Inc.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     Firestar Software, Inc. - initial API and implementation
- *
- * Author:
- *     Gabriel Oancea
- *
- *******************************************************************************/
+* Copyright (c) 2012 Firestar Software, Inc.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     Firestar Software, Inc. - initial API and implementation
+*
+* Author:
+*     Gabriel Oancea
+*
+*******************************************************************************/
 package org.openhealthtools.mdht.mdmi.engine.converter.to;
 
 import java.text.*;
 import java.util.*;
 
 import org.openhealthtools.mdht.mdmi.*;
-import org.openhealthtools.mdht.mdmi.engine.converter.DateWrapper;
 import org.openhealthtools.mdht.mdmi.util.*;
 
 public class ToDateTimeConverter implements IConvertToString {
 
-    @Override
-    public String convertToString(Object obj, String format) {
-        if (!((obj instanceof Date) || obj instanceof DateWrapper))
-            throw new IllegalArgumentException("Object is not a java.util.Date type.");
-        try {
-            if (format != null && 0 < format.length() && !format.equals("DATETIME")) {
-                String[] formatStrings = format.split(";");
-                if (obj instanceof DateWrapper) {
-                    DateWrapper dateWrapper = (DateWrapper) obj;
-                    obj = dateWrapper.getDate();
-                    String orgFormat = dateWrapper.getOriginalFormat();
-                    if (orgFormat != null && Arrays.asList(formatStrings).contains(orgFormat))
-                        return convert(obj, orgFormat);
-                }
+   @Override
+   public String convertToString( Object obj, String format ) {
+      if( !(obj instanceof Date) )
+         throw new IllegalArgumentException("Object is not a java.util.Date type.");
 
-                for (String formatString : formatStrings) {
-                    String convert = convert(obj, formatString);
-                    if (convert != null) return convert;
-                }
-            }
-        } catch (Exception ex) {
-            throw new MdmiException(ex, "ToDateTimeConverter.convertToString({0}, {1}) failed.", obj, format);
-        }
-        return XmlUtil.formatDateYMDHMSMZ((Date) obj);
-    }
+      if( format != null && 0 < format.length() && !format.equals("DATETIME") ) {
+          String[] formatStrings = format.split(";");
+          for (String formatString : formatStrings) {
+              try {
+                  SimpleDateFormat sdf = new SimpleDateFormat(formatString);
+                  return sdf.format((Date)obj);
+              }
+              catch (IllegalArgumentException ignored) {}
+              catch( Exception ex ) {
+                  throw new MdmiException(ex, "ToDateTimeConverter.convertToString({0}, {1}) failed.", obj, format);
+              }
+          }
 
-    private String convert(Object obj, String format) {
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat(format);
-            return sdf.format((Date) obj);
-        } catch (IllegalArgumentException ignored) {
-        }
-        return null;
-    }
+      }
+
+      return XmlUtil.formatDateYMDHMSMZ((Date)obj);
+   }
+
 }
