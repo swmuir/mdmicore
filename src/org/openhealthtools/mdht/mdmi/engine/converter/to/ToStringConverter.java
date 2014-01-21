ca90e08260ccdecc7f46b6ecdf0359be4d29b64c
jklann <jklann@b3b0ff50-1bf6-4f1c-ad2b-09b492ebfb26>
2014-01-21 00:46:32 +0000

    Now converting a Date to a String allows semicolon separated format strings in the syntax node

--- src/org/openhealthtools/mdht/mdmi/engine/converter/to/ToStringConverter.java
+++ src/org/openhealthtools/mdht/mdmi/engine/converter/to/ToStringConverter.java
@@ -14,9 +14,36 @@
 *******************************************************************************/
 package org.openhealthtools.mdht.mdmi.engine.converter.to;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.openhealthtools.mdht.mdmi.engine.converter.DateWrapper;
+
 public class ToStringConverter implements IConvertToString {
    @Override
    public String convertToString( Object obj, String format ) {
+	   if (format!=null && format.length()>0) {
+	    if (obj instanceof Date) return convertStringSplit(obj, format);
+	    if (obj instanceof DateWrapper) return convertStringSplit(((DateWrapper)obj).getDate(),format);
+	   }
       return obj.toString();
+   }
+
+   public String convertStringSplit(Object obj, String format) {
+	   String[] formatStrings = format.split(";");
+       for (String formatString : formatStrings) {
+           String convert = convert(obj, formatString);
+           if (convert != null) return convert;
+       }
+	return null;
+   }
+
+   private String convert(Object obj, String format) {
+       try {
+           SimpleDateFormat sdf = new SimpleDateFormat(format);
+           return sdf.format((Date) obj);
+       } catch (IllegalArgumentException ignored) {
+       }
+       return null;
    }
 }
