effa8907ced0b7f0470594bcfde42cedc799e4b6
seanmuir <seanmuir@b3b0ff50-1bf6-4f1c-ad2b-09b492ebfb26>
2014-03-12 17:12:56 +0000

    modifed resolver to us map

--- src/org/openhealthtools/mdht/mdmi/MdmiExternalResolvers.java
+++ src/org/openhealthtools/mdht/mdmi/MdmiExternalResolvers.java
@@ -26,6 +26,7 @@ import org.openhealthtools.mdht.mdmi.util.*;
  */
 public final class MdmiExternalResolvers {
    private final ArrayList<IExternalResolver> m_resolvers = new ArrayList<IExternalResolver>();
+   private final HashMap<DTExternal,IExternalResolver> m_resolversMap = new HashMap<DTExternal,IExternalResolver>();
 
    MdmiExternalResolvers( MdmiConfig config ) {
       loadResolvers(config);
@@ -46,6 +47,18 @@ public final class MdmiExternalResolvers {
       }
    }
    
+    private void setResolvers(DTExternal datatype){
+   	  for( IExternalResolver resolver : m_resolvers ) {         
+           if( resolver.canHandle(datatype) ) {
+              m_resolversMap.put(datatype,resolver);
+              return;
+           }
+        }
+   	  Mdmi.INSTANCE.logger().warning("A resolver was not found for data type: " + datatype.getName());
+   	  m_resolversMap.put(datatype,null);
+   	
+   }
+   
    /**
     * Find a resolver that can handle the given datatype and value, and call its getValue()
     * method and return the value. If none found will return the given value.
@@ -58,13 +71,17 @@ public final class MdmiExternalResolvers {
    public Object getDictionaryValue( DTExternal datatype, String value ) {
       if( datatype == null )
          throw new IllegalArgumentException("Datatype is null");
-      for( int i = 0; i < m_resolvers.size(); i++ ) {
-         IExternalResolver p = m_resolvers.get(i);
-         if( p.canHandle(datatype) ) {
+      
+      if (!m_resolversMap.containsKey(datatype)) {
+      	 setResolvers( datatype);
+      }
+     
+         IExternalResolver p =m_resolversMap.get(datatype);
+         if( p != null ) {
             return p.getDictionaryValue(datatype, value);
          }
-      }
-      Mdmi.INSTANCE.logger().warning("A resolver was not found for data type: " + datatype.getName());
+   
+  
       return value;
    }
    
@@ -80,12 +97,16 @@ public final class MdmiExternalResolvers {
    public String getModelValue( DTExternal datatype, Object value ) {
       if( datatype == null )
          throw new IllegalArgumentException("Datatype is null");
-      for(  IExternalResolver p : m_resolvers ) {
-         if( p.canHandle(datatype) ) {
+     
+      if (!m_resolversMap.containsKey(datatype)) {
+     	 setResolvers( datatype);
+     }
+    
+        IExternalResolver p =m_resolversMap.get(datatype);
+        if( p != null ) {
             return p.getModelValue(datatype, value);
-         }
-      } 
-      Mdmi.INSTANCE.logger().warning("A resolver was not found for data type: " + datatype.getName());
+        }
+   
       return value == null ? null : value.toString();
    }
 } // MdmiExternalResolvers
